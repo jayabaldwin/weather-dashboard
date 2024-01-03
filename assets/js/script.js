39e4715052ef9da8a30f1a4eb346d4a39b019986
Jaya <148409249+jayabaldwin@users.noreply.github.com>
2024-01-04 09:57:37 +1100

    initial JS development for fetching data

---
+++ assets/js/script.js
@@ -0,0 +1,45 @@
+var search = 'sydney';
+// API Key
+var weatherKey = "735ee00c033a0c203ee912145178a36b";
+
+// the API url
+var searchUrl = `https://api.openweathermap.org/geo/1.0/direct?q=${search}&limit=5&appid=${weatherKey}`;
+
+// var weatherApiStart = 'https://api.openweathermap.org/geo/1.0/direct?q=';
+// var weatherApiEnd = `&limit=5&appid=${weatherKey}`;
+// fetch(weatherApiStart + search + weatherApiEnd)
+
+function getWeatherData() {
+
+
+
+fetch(searchUrl)
+  .then(function (response) {
+    return response.json();
+  })
+    .then(function (data) {
+        console.log(data[0].lon);
+        var lat = data[0].lat;
+        var lon = data[0].lon;
+        var weatherUrl = `https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=${weatherKey}`;
+
+        fetch(weatherUrl)
+            .then(function (response) {
+            return response.json();
+        })  
+            .then(function (weatherInfo) {
+                    //this is where you have all your important data!
+                    console.log(weatherInfo)
+                    
+                    for (var i = 0; i < weatherInfo.list; i++) {
+                       const pTag = document.createElement("p")
+                       pTag.innerHTMl = weatherInfo.list[0].main.temp
+                       pTag.append(someDiv)
+
+                   } 
+            }) 
+
+});
+}
+
+getWeatherData()
