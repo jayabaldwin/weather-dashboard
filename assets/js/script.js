a0ac2e8adb00c73e415b33eaa225c759cbe2bb50
Jaya <148409249+jayabaldwin@users.noreply.github.com>
2024-01-18 15:41:52 +1100

    fixed issue with search

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,16 +6,14 @@ const searchInput = document.getElementById('inputText');
 const searchButton = document.getElementById('search-btn');
 const pastSearches = document.getElementById('past-searches');
 const searchForm = document.querySelector('#form');
-
-var search = searchInput.value;
-
+var search = '';
 
 // API Key
 var apiKey = "735ee00c033a0c203ee912145178a36b";
 var baseUrl = "https://api.openweathermap.org";
 
 // Fetching the users' location to be able to generate weather data
-function fetchLocation() {
+function fetchLocation(search) {
   var apiUrl = `${baseUrl}/geo/1.0/direct?q=${search}&limit=5&units=metric&appid=${apiKey}`;
 
   fetch(apiUrl)
@@ -23,6 +21,7 @@ function fetchLocation() {
       return res.json();
     })
     .then(function (data) {
+      console.log('data', data);
       var lat = data[0].lat;
       var lon = data[0].lon;
 
@@ -42,39 +41,54 @@ function fetchLocation() {
 
 
 // Find way to remove the time element from the string with a split() method
-function separateDateAndTime () {
+function separateDateAndTime() {
+}
+
+// How do I multiply wind by 3.6 to generate km/hr instead of m/s
+function windSpeed() {
 }
 
 // Sets up ability to update each forecast day without repetition
 function updateWeatherElement(id, label, value) {
-  document.getElementById(id).innerHTML = `${label}: ${value}`;
+  document.getElementById(id).innerHTML = `${label} ${value}`;
 }
 
 // Data collected from the object
 function weatherResults(weatherData) {
   // Update current day
-  updateWeatherElement('city-date', 'Date', `${weatherData.city.name} (${weatherData.list[0].dt_txt})`);
-  updateWeatherElement('temp-0', 'Temp', `${weatherData.list[0].main.temp}°C`);
-  updateWeatherElement('wind-0', 'Wind', `${weatherData.list[0].wind.speed} km/h`);
-  updateWeatherElement('humidity-0', 'Humidity', `${weatherData.list[0].main.humidity}%`);
+  updateWeatherElement('city-date', '', `${weatherData.city.name} (${weatherData.list[0].dt_txt})`);
+  updateWeatherElement('temp-0', 'Temperature:', `${weatherData.list[0].main.temp}°C`);
+  updateWeatherElement('wind-0', 'Wind:', `${weatherData.list[0].wind.speed} km/h`);
+  updateWeatherElement('humidity-0', 'Humidity:', `${weatherData.list[0].main.humidity}%`);
 
   // Update subsequent days
   for (let i = 1; i <= 4; i++) {
     const index = i * 8;
-    updateWeatherElement(`day-${i}-date`, 'Date', `(${weatherData.list[index].dt_txt})`);
-    updateWeatherElement(`temp-${i}`, 'Temp', `${weatherData.list[index].main.temp}°C`);
-    updateWeatherElement(`wind-${i}`, 'Wind', `${weatherData.list[index].wind.speed} km/h`);
-    updateWeatherElement(`humidity-${i}`, 'Humidity', `${weatherData.list[index].main.humidity}%`);
+    updateWeatherElement(`day-${i}-date`, '', `(${weatherData.list[index].dt_txt})`);
+    updateWeatherElement(`temp-${i}`, 'Temp:', `${weatherData.list[index].main.temp}°C`);
+    updateWeatherElement(`wind-${i}`, 'Wind:', `${weatherData.list[index].wind.speed} km/h`);
+    updateWeatherElement(`humidity-${i}`, 'Humidity:', `${weatherData.list[index].main.humidity}%`);
+
+    var weatherImage = document.getElementById('weather-icon');
+
+    if(`${weatherData.list[index].weather[0].main}` == "Rain") {
+      
+    }
   }
+
 
   console.log(weatherData);
 }
 
+// Storing searches and appending them to buttons
+function previousSearches() {
+}
 
 // Submission of form
 function handleSearchFormSubmit (event) {
+  // console.log(event.target[0].value);
   event.preventDefault();
-  var search = searchInput.value;
+  var search = event.target[0].value;
   fetchLocation(search);
 }
 
